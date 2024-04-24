5439434c1daba29be270f974eef16531d7851b33
therealjacobmartin <jacob.martin@richmond.edu>
2024-04-24 19:34:56 -0400

    little bit of API stuff

--- EventPlannerAPI.cpp
+++ EventPlannerAPI.cpp
@@ -6,12 +6,46 @@
 #include "Event.h"
 #include "PastEvent.h"
 #include "artistFunctions.h"
+#include "venueFunctions.h"
+#include "templateSaving.h"
 
 using namespace std;
 using namespace crow;
 
 //map to store Artists
-map<string, Artist> artistMap;
+map<string, Artist> artistMap = loadFromFile<Artist>("artists.json");
+//map to store Venues
+map<string, Venue> venueMap = loadFromFile<Venue>("venues.json");
+//map to store future Events
+map<string, Event> futureMap = loadFromFile<Event>("events.json");
+//map to store Past Events
+map<string, PastEvent> pastMap;
 
-template <typename T>
-map<string, Venue<T>> venueMap;
+int main()
+{
+
+    SimpleApp app;
+
+
+    // Artists
+    CROW_ROUTE(app, "/api/artists").methods("POST"_method)(createArtist);
+    CROW_ROUTE(app, "/api/artists").methods("GET"_method)(readAllArtists);
+    CROW_ROUTE(app, "/api/artists/<string>").methods("GET"_method)(readArtist);
+    CROW_ROUTE(app, "/api/artists/<string>").methods("PUT"_method)(updateArtist);
+    CROW_ROUTE(app, "/api/artists/<string>").methods("DELETE"_method)(deleteArtist);
+
+    // Venues
+    CROW_ROUTE(app, "/api/venues").methods("POST"_method)(createVenue);
+    CROW_ROUTE(app, "/api/venues").methods("GET"_method)(readAllVenues);
+    CROW_ROUTE(app, "/api/venues/<string>").methods("GET"_method)(readVenue);
+    CROW_ROUTE(app, "/api/venues/<string>").methods("PUT"_method)(updateVenue);
+    CROW_ROUTE(app, "/api/venues/<string>").methods("DELETE"_method)(deleteVenue);
+
+
+    app.port(19455).run();
+
+    saveToFile<Artist>(artistMap, "artists.json");
+    saveToFile<Venue>(venueMap, "venues.json");
+    saveToFile<Event>(futureMap, "events.json");
+
+}
---
+++ venueFunctions.h
@@ -0,0 +1,16 @@
+#ifndef VENUE_FUNCTIONS_H
+#define VENUE_FUNCTIONS_H
+
+#include <crow.h>
+#include <map>
+#include <string>
+#include "Venue.h"
+
+//Functions for GET/POST/PUT/DELETE
+crow::response createVenue(crow::request req);
+crow::response readVenue(std::string name);
+crow::response readAllVenues(crow::request req);
+void updateVenue(crow::request req, crow::response& res, std::string name);
+crow::response deleteVenue(std::string name);
+
+#endif
